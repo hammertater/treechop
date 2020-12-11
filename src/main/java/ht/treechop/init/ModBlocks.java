91e367a4d501140c3da36bd36ee8706f58420b3d
hammertater <74752031+hammertater@users.noreply.github.com>
2020-12-11 15:46:35 -0500

    Clean up

--- src/main/java/ht/treechop/init/ModBlocks.java
+++ src/main/java/ht/treechop/init/ModBlocks.java
@@ -25,9 +25,4 @@ public class ModBlocks {
                             .sound(SoundType.WOOD))
     );
 
-//    static {
-//        List<Block> choppableBlocks = BlockTags.createOptional(ConfigHandler.blockTagForDetectingLogs).getAllElements();
-//        choppableBlocks.
-//    }
-
 }
