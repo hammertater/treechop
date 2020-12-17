05628bf84726d6ca4db9d696cc243d1c74636567
hammertater <74752031+hammertater@users.noreply.github.com>
2020-12-17 14:27:07 -0500

    Fix chopped log collision boxes and rendering

--- src/main/java/ht/treechop/common/block/ChoppedLogBlock.java
+++ src/main/java/ht/treechop/common/block/ChoppedLogBlock.java
@@ -12,10 +12,14 @@ import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static ht.treechop.common.util.ChopUtil.isBlockChoppable;
 
@@ -25,6 +29,114 @@ public class ChoppedLogBlock extends Block implements IChoppable {
 
     private static final PropertyEnum<ChoppedLogShape> SHAPE = BlockStateProperties.CHOPPED_LOG_SHAPE;
     private static final PropertyInteger CHOPS = BlockStateProperties.CHOP_COUNT;
+
+    public static final AxisAlignedBB[] PILLAR_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] CORNER_NW_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] CORNER_NE_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] CORNER_SE_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] CORNER_SW_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] END_W_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] END_N_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] END_E_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] END_S_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] SIDE_W_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] SIDE_N_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] SIDE_E_SHAPES_BY_CHOPS;
+    public static final AxisAlignedBB[] SIDE_S_SHAPES_BY_CHOPS;
+
+    static {
+        List<Float> scaledChops = Stream.of(0F, 1F, 2F, 3F, 4F, 5F, 6F, 7F).map(a -> a / 16).collect(Collectors.toList());
+        PILLAR_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        chops, 0, chops, 1 - chops,
+                        1, 1 - chops
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        CORNER_NW_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        0, 0, 0,
+                        1 - chops * 2, 1, 1 - chops * 2
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        CORNER_NE_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        chops * 2, 0, 0,
+                        1, 1, 1 - chops * 2
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        CORNER_SE_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        chops * 2, 0, chops * 2,
+                        1, 1, 1
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        CORNER_SW_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        0, 0, chops * 2,
+                        1 - chops * 2, 1, 1
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        END_W_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        chops * 2, 0, chops,
+                        1, 1, 1 - chops
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        END_N_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        chops, 0, chops * 2,
+                        1 - chops, 1, 1
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        END_E_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        0, 0, chops,
+                        1 - chops * 2, 1, 1 - chops
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        END_S_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        chops, 0, 0,
+                        1 - chops, 1, 1 - chops * 2
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        SIDE_W_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        chops * 2, 0, 0,
+                        1, 1, 1
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        SIDE_N_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        0, 0, chops * 2,
+                        1, 1, 1
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        SIDE_E_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        0, 0, 0,
+                        1 - chops * 2, 1, 1
+                ))
+                .toArray(AxisAlignedBB[]::new);
+
+        SIDE_S_SHAPES_BY_CHOPS = scaledChops.stream()
+                .map(chops -> new AxisAlignedBB(
+                        0, 0, 0,
+                        1, 1, 1 - chops * 2
+                ))
+                .toArray(AxisAlignedBB[]::new);
+    }
 
     public ChoppedLogBlock() {
         super(Material.WOOD, MapColor.WOOD);
@@ -36,6 +148,15 @@ public class ChoppedLogBlock extends Block implements IChoppable {
                 .withProperty(SHAPE, ChoppedLogShape.PILLAR)
                 .withProperty(CHOPS, 1)
         );
+    }
+
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
     }
 
     @Override
@@ -70,6 +191,38 @@ public class ChoppedLogBlock extends Block implements IChoppable {
     @Override
     public int getMaxNumChops() {
         return MAX_NUM_CHOPS;
+    }
+
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        int chops = state.getValue(CHOPS);
+        switch (state.getValue(SHAPE)) {
+            case CORNER_NW:
+                return CORNER_NW_SHAPES_BY_CHOPS[chops];
+            case CORNER_NE:
+                return CORNER_NE_SHAPES_BY_CHOPS[chops];
+            case CORNER_SE:
+                return CORNER_SE_SHAPES_BY_CHOPS[chops];
+            case CORNER_SW:
+                return CORNER_SW_SHAPES_BY_CHOPS[chops];
+            case END_W:
+                return END_W_SHAPES_BY_CHOPS[chops];
+            case END_N:
+                return END_N_SHAPES_BY_CHOPS[chops];
+            case END_E:
+                return END_E_SHAPES_BY_CHOPS[chops];
+            case END_S:
+                return END_S_SHAPES_BY_CHOPS[chops];
+            case SIDE_W:
+                return SIDE_W_SHAPES_BY_CHOPS[chops];
+            case SIDE_N:
+                return SIDE_N_SHAPES_BY_CHOPS[chops];
+            case SIDE_E:
+                return SIDE_E_SHAPES_BY_CHOPS[chops];
+            case SIDE_S:
+                return SIDE_S_SHAPES_BY_CHOPS[chops];
+            default:
+                return PILLAR_SHAPES_BY_CHOPS[chops];
+        }
     }
 
     public static ChoppedLogShape getPlacementShape(World world, BlockPos blockPos) {
