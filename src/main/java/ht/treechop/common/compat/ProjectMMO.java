4ef8bd35bf5d9124f6e724bba332168729602e77
hammertater <74752031+hammertater@users.noreply.github.com>
2021-01-23 22:57:03 -0500

    Clean stuff

--- src/main/java/ht/treechop/common/compat/ProjectMMO.java
+++ src/main/java/ht/treechop/common/compat/ProjectMMO.java
@@ -1,8 +1,10 @@
 package ht.treechop.common.compat;
 
 import harmonised.pmmo.events.BlockBrokenHandler;
+import ht.treechop.TreeChopMod;
 import ht.treechop.common.config.ConfigHandler;
 import ht.treechop.common.event.ChopEvent;
+import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,7 +12,7 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
-@EventBusSubscriber
+@EventBusSubscriber(modid = TreeChopMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class ProjectMMO {
 
     @SubscribeEvent
@@ -26,7 +28,7 @@ public class ProjectMMO {
             BlockBrokenHandler.handleBroken(new BlockEvent.BreakEvent(
                     event.getWorld(),
                     event.getChoppedBlockPos(),
-                    event.getChoppedBlockState(),
+                    Blocks.OAK_LOG.getDefaultState(), // TODO: use correct wood type
                     event.getPlayer()
             ));
         }
