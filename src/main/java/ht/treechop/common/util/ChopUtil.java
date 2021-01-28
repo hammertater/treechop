73ecc420d0bf8cb00a056adbd265d1dd381de650
hammertater <74752031+hammertater@users.noreply.github.com>
2021-01-27 23:13:12 -0500

    Update changelog

--- src/main/java/ht/treechop/common/util/ChopUtil.java
+++ src/main/java/ht/treechop/common/util/ChopUtil.java
@@ -414,7 +414,9 @@ public class ChopUtil {
 
     @SuppressWarnings("ConstantConditions")
     public static ChopSettings getPlayerChopSettings(PlayerEntity player) {
-        return isLocalPlayer(player) ? Client.getChopSettings() : ChopSettingsCapability.forPlayer(player).orElse(new ChopSettingsCapability());
+        return isLocalPlayer(player)
+                ? Client.getChopSettings()
+                : ChopSettingsCapability.forPlayer(player).orElse(new ChopSettingsCapability());
     }
 
     public static void doItemDamage(ItemStack itemStack, World world, BlockState blockState, BlockPos blockPos, PlayerEntity agent) {
