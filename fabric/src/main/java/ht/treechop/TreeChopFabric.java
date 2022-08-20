cf52bc55f45572d9230e5d3ba72bae7a6356f641
hammertater <74752031+hammertater@users.noreply.github.com>
2022-08-20 00:34:57 -0400

    Some Fabric implementations

--- fabric/src/main/java/ht/treechop/TreeChopFabric.java
+++ fabric/src/main/java/ht/treechop/TreeChopFabric.java
@@ -1,6 +1,7 @@
 package ht.treechop;
 
 import ht.treechop.common.Common;
+import ht.treechop.common.platform.FabricPlatform;
 import ht.treechop.common.registry.FabricModBlocks;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
@@ -11,6 +12,8 @@ public class TreeChopFabric extends TreeChop implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        platform = new FabricPlatform();
+
         PlayerBlockBreakEvents.BEFORE.register(Common::onBreakEvent);
 
         Registry.register(Registry.BLOCK, new ResourceLocation(MOD_ID, "chopped_log"), FabricModBlocks.CHOPPED_LOG);
