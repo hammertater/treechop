504178e6a7eba03618ad3fe82f3b6f840e31d972
hammertater <74752031+hammertater@users.noreply.github.com>
2022-10-10 15:18:50 -0400

    Better Sodium rendering, fix Starlight conflicts #130

--- fabric/src/main/java/ht/treechop/client/model/FabricChoppedLogEntityRenderer.java
+++ fabric/src/main/java/ht/treechop/client/model/FabricChoppedLogEntityRenderer.java
@@ -2,16 +2,22 @@ package ht.treechop.client.model;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import ht.treechop.client.FabricClient;
 import ht.treechop.common.block.FabricChoppedLogBlock;
 import ht.treechop.common.util.ChopUtil;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderContext;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.Random;
 import java.util.Set;
@@ -19,6 +25,7 @@ import java.util.Set;
 public class FabricChoppedLogEntityRenderer extends FabricChoppedLogBakedModel implements BlockEntityRenderer<FabricChoppedLogBlock.MyEntity> {
     protected final TextureAtlasSprite defaultSprite;
     private final Random random = new Random();
+    private final BlockRenderContext renderContext = new BlockRenderContext();
 
     public FabricChoppedLogEntityRenderer(BlockEntityRendererProvider.Context context) {
         this.defaultSprite = Minecraft.getInstance().getModelManager()
@@ -27,16 +34,13 @@ public class FabricChoppedLogEntityRenderer extends FabricChoppedLogBakedModel i
     }
 
     @Override
-    public void render(FabricChoppedLogBlock.MyEntity entity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, int somethin1, int somethin2) {
-        VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.solid());
-        Set<Direction> solidSides = entity.getShape().getSolidSides(entity.getLevel(), entity.getBlockPos());
-
-        final float RGB = 1f;
-        getQuads(ChopUtil.getStrippedState(entity.getOriginalState()),
-                entity.getShape(),
-                entity.getChops(),
-                solidSides,
-                random)
-                .forEach(quad -> vertexConsumer.putBulkData(poseStack.last(), quad, RGB, RGB, RGB, somethin1, somethin2));
+    public void render(FabricChoppedLogBlock.MyEntity entity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay) {
+        Level level = entity.getLevel();
+        if (level != null) {
+            BlockPos pos = entity.getBlockPos();
+            BlockState blockState = entity.getLevel().getBlockState(pos);
+            VertexConsumer vertexConsumer = multiBufferSource.getBuffer(RenderType.solid());
+            renderContext.render(level, FabricClient.choppedLogModel, blockState, pos, poseStack, vertexConsumer, random, 0L, overlay);
+        }
     }
 }
