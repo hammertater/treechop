f2d361964264923b4ad82dedfb5992463d300ccf
hammertater <74752031+hammertater@users.noreply.github.com>
2022-10-24 18:31:08 -0400

    Add docs

--- shared/src/main/java/ht/treechop/api/IFellableBlock.java
+++ shared/src/main/java/ht/treechop/api/IFellableBlock.java
@@ -6,6 +6,15 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public interface IFellableBlock {
 
+    /**
+     * Determines the amount that this block contributes to the "size" of a tree, which determines the number of chops
+     * required to fell a tree.
+     *
+     * @param level
+     * @param pos
+     * @param blockState
+     * @return
+     */
     double getSupportFactor(BlockGetter level, BlockPos pos, BlockState blockState);
 
 }
