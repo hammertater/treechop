aa8c7393dc235d311a5f28d471bbbe4409944d9e
hammertater <74752031+hammertater@users.noreply.github.com>
2022-10-24 19:41:58 -0400

    Set up tests project

---
+++ tests/fabric-tests/src/main/java/ht/treechoptests/TreeChopFabricTests.java
@@ -0,0 +1,9 @@
+package ht.treechoptests;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TreeChopFabricTests implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
