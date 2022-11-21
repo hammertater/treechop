a93677c1d40883901f88ac96860a36ae61bcd2fb
hammertater <74752031+hammertater@users.noreply.github.com>
2022-11-20 23:53:28 -0500

    Restore Jade and Wthit compat on Forge

---
+++ forge/src/main/java/ht/treechop/compat/Jade.java
@@ -0,0 +1,100 @@
+package ht.treechop.compat;
+
+import ht.treechop.TreeChop;
+import ht.treechop.api.TreeData;
+import ht.treechop.client.Client;
+import ht.treechop.common.block.ChoppedLogBlock;
+import ht.treechop.common.util.ChopUtil;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec2;
+import snownee.jade.api.*;
+import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.IElement;
+import snownee.jade.impl.ui.ItemStackElement;
+
+import javax.annotation.Nullable;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@WailaPlugin
+public class Jade implements IWailaPlugin, IBlockComponentProvider {
+    private static final Jade INSTANCE = new Jade();
+    private static final ResourceLocation SHOW_TREE_BLOCKS = new ResourceLocation(TreeChop.MOD_ID, "show_tree_block_counts");
+    private static final ResourceLocation SHOW_NUM_CHOPS_REMAINING = new ResourceLocation(TreeChop.MOD_ID, "show_num_chops_remaining");
+
+    public Jade() {
+    }
+
+    @Override
+    public void registerClient(IWailaClientRegistration registrar) {
+        registrar.registerBlockComponent(INSTANCE, Block.class);
+        registrar.addConfig(SHOW_TREE_BLOCKS, true);
+        registrar.addConfig(SHOW_NUM_CHOPS_REMAINING, true);
+    }
+
+    @Override
+    public @Nullable
+    IElement getIcon(BlockAccessor accessor, IPluginConfig config, IElement currentIcon) {
+        BlockState state = getLogState(accessor.getLevel(), accessor.getPosition(), accessor.getBlockState());
+        return ItemStackElement.of(
+                (state != accessor.getBlockState())
+                        ? state.getBlock().asItem().getDefaultInstance()
+                        : Items.OAK_LOG.getDefaultInstance()
+        );
+    }
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+        if (ChopUtil.playerWantsToChop(accessor.getPlayer(), Client.getChopSettings())
+                && ChopUtil.isBlockChoppable(accessor.getLevel(), accessor.getPosition(), accessor.getBlockState())
+                && (config.get(SHOW_TREE_BLOCKS) || config.get(SHOW_NUM_CHOPS_REMAINING))) {
+            Level level = accessor.getLevel();
+            AtomicInteger numChops = new AtomicInteger(0);
+
+            TreeData tree = Client.treeCache.getTree(level, accessor.getPosition());
+            if (tree.isAProperTree(Client.getChopSettings().getTreesMustHaveLeaves())) {
+                tree.getLogBlocks().ifPresent(
+                        treeBlocks -> {
+                            if (config.get(SHOW_NUM_CHOPS_REMAINING)) {
+                                treeBlocks.forEach((BlockPos pos) -> numChops.getAndAdd(ChopUtil.getNumChops(level, pos)));
+                                tooltip.add(Component.translatable("treechop.waila.x_out_of_y_chops", numChops.get(), ChopUtil.numChopsToFell(treeBlocks.size())));
+                            }
+
+                            if (config.get(SHOW_TREE_BLOCKS)) {
+                                LinkedList<IElement> tiles = new LinkedList<>();
+                                treeBlocks.stream()
+                                        .collect(Collectors.groupingBy((BlockPos pos) -> {
+                                            BlockState state = level.getBlockState(pos);
+                                            return getLogState(level, pos, state).getBlock();
+                                        }, Collectors.counting()))
+                                        .forEach((block, count) -> {
+                                            IElement icon = tooltip.getElementHelper().item(block.asItem().getDefaultInstance(), 1f, count.toString());
+                                            tiles.add(icon.translate(new Vec2(0, -1.5f)));
+                                        });
+                                tooltip.add(tiles);
+                            }
+                        });
+            }
+        }
+    }
+
+    private BlockState getLogState(Level level, BlockPos pos, BlockState state) {
+        if (level.getBlockEntity(pos) instanceof ChoppedLogBlock.MyEntity entity) {
+            return entity.getOriginalState();
+        } else {
+            return state;
+        }
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return TreeChop.resource("plugin");
+    }
+}
---
+++ forge/src/main/java/ht/treechop/compat/Wthit.java
@@ -0,0 +1,85 @@
+package ht.treechop.compat;
+
+import ht.treechop.TreeChop;
+import ht.treechop.api.TreeData;
+import ht.treechop.client.Client;
+import ht.treechop.common.block.ChoppedLogBlock;
+import ht.treechop.common.util.ChopUtil;
+import mcp.mobius.waila.api.*;
+import mcp.mobius.waila.api.component.ItemComponent;
+import mcp.mobius.waila.api.component.WrappedComponent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+import javax.annotation.Nullable;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class Wthit implements IWailaPlugin, IBlockComponentProvider {
+    private static final Wthit INSTANCE = new Wthit();
+
+    public static final ResourceLocation SHOW_TREE_BLOCKS = new ResourceLocation(TreeChop.MOD_ID, "show_tree_block_counts");
+    public static final ResourceLocation SHOW_NUM_CHOPS_REMAINING = new ResourceLocation(TreeChop.MOD_ID, "show_num_chops_remaining");
+
+    @Override
+    public void register(IRegistrar registrar) {
+        registrar.addConfig(SHOW_TREE_BLOCKS, true);
+        registrar.addConfig(SHOW_NUM_CHOPS_REMAINING, true);
+        registrar.addComponent(INSTANCE, TooltipPosition.BODY, Block.class);
+        registrar.addOverride(INSTANCE, ChoppedLogBlock.class);
+    }
+
+    @Override
+    public @Nullable
+    BlockState getOverride(IBlockAccessor accessor, IPluginConfig config) {
+        return getLogState(accessor.getWorld(), accessor.getPosition(), accessor.getBlockState());
+    }
+
+    @Override
+    public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
+        if (ChopUtil.playerWantsToChop(accessor.getPlayer())
+                && ChopUtil.isBlockChoppable(accessor.getWorld(), accessor.getPosition(), accessor.getBlockState())
+                && (config.getBoolean(SHOW_TREE_BLOCKS) || config.getBoolean(SHOW_NUM_CHOPS_REMAINING))) {
+            Level level = accessor.getWorld();
+            AtomicInteger numChops = new AtomicInteger(0);
+
+            TreeData tree = Client.treeCache.getTree(level, accessor.getPosition());
+            if (tree.isAProperTree(Client.getChopSettings().getTreesMustHaveLeaves())) {
+                tree.getLogBlocks().ifPresent(
+                        treeBlocks -> {
+                            if (config.getBoolean(SHOW_NUM_CHOPS_REMAINING)) {
+                                treeBlocks.forEach((BlockPos pos) -> numChops.getAndAdd(ChopUtil.getNumChops(level, pos)));
+                                tooltip.addLine(new WrappedComponent(Component.translatable("treechop.waila.x_out_of_y_chops", numChops.get(), ChopUtil.numChopsToFell(treeBlocks.size()))));
+                            }
+
+                            if (config.getBoolean(SHOW_TREE_BLOCKS)) {
+                                ITooltipLine line = tooltip.addLine();
+                                treeBlocks.stream()
+                                        .collect(Collectors.groupingBy((BlockPos pos) -> {
+                                            BlockState state = level.getBlockState(pos);
+                                            return getLogState(level, pos, state).getBlock();
+                                        }, Collectors.counting()))
+                                        .forEach((block, count) -> {
+                                            ItemStack stack = block.asItem().getDefaultInstance();
+                                            stack.setCount(count.intValue());
+                                            line.with(new ItemComponent(stack));
+                                        });
+                            }
+                        });
+            }
+        }
+    }
+
+    private BlockState getLogState(Level level, BlockPos pos, BlockState state) {
+        if (level.getBlockEntity(pos) instanceof ChoppedLogBlock.MyEntity entity) {
+            return entity.getOriginalState();
+        } else {
+            return state;
+        }
+    }
+}
