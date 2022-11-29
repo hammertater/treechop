5b8639eea9a0706407087ef75e9d01feeb08eca6
hammertater <74752031+hammertater@users.noreply.github.com>
2022-11-29 14:42:01 -0500

    Remove outdated warning

--- fabric/src/main/java/ht/treechop/client/FabricClient.java
+++ fabric/src/main/java/ht/treechop/client/FabricClient.java
@@ -31,7 +31,7 @@ public class FabricClient extends Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         if (FabricLoader.getInstance().isModLoaded("sodium")) {
-            TreeChop.LOGGER.info("Sodium detected! Using alternative block renderer (chopped blocks will not use vanilla Minecraft's ambient occlusion).");
+            TreeChop.LOGGER.info("Sodium detected! Using alternative block renderer.");
             ModelLoadingRegistry.INSTANCE.registerResourceProvider(resourceManager -> new ChoppedLogModelProvider(new HiddenChoppedLogBakedModel()));
             BlockEntityRendererRegistry.register(FabricModBlocks.CHOPPED_LOG_ENTITY, FabricChoppedLogEntityRenderer::new);
         } else {
