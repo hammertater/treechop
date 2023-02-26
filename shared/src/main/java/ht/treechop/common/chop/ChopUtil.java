269a2461e382624af5b5de77a8f17e14a25175c3
hammertater <74752031+hammertater@users.noreply.github.com>
2023-02-26 16:23:22 -0500

    Trigger item block mining effects #172

--- shared/src/main/java/ht/treechop/common/chop/ChopUtil.java
+++ shared/src/main/java/ht/treechop/common/chop/ChopUtil.java
@@ -439,10 +439,7 @@ public class ChopUtil {
         if (chopResult != ChopResult.IGNORED) {
             boolean felled = chopResult.apply(pos, agent, tool, ConfigHandler.COMMON.breakLeaves.get());
             TreeChop.platform.finishChopEvent(agent, level, pos, blockState, chopData, felled);
-
-            if (!agent.isCreative()) {
-                tool.hurtAndBreak(1, agent, ignored -> {});
-            }
+            tool.mineBlock(level, blockState, pos, agent);
 
             return !felled;
         }
