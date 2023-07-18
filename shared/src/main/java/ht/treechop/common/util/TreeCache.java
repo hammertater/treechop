4edc78e4f06e3baca335ea896eb9c6ef22e19d0e
hammertater <74752031+hammertater@users.noreply.github.com>
2023-07-17 21:36:13 -0400

    Fix stale tree cache

--- shared/src/main/java/ht/treechop/common/util/TreeCache.java
+++ shared/src/main/java/ht/treechop/common/util/TreeCache.java
@@ -7,7 +7,10 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.Objects;
 
 public class TreeCache {
     private final SingleBlockCache<TreeData> singleBlockCache = new SingleBlockCache<>();
@@ -37,9 +40,10 @@ public class TreeCache {
         private BlockGetter level;
         private BlockState blockState;
         private BlockPos pos;
+        private BlockEntity entity;
 
         public T get(BlockGetter level, BlockPos pos) {
-            if (level == this.level && pos.equals(this.pos) && level.getBlockState(pos).equals(blockState)) {
+            if (level == this.level && pos.equals(this.pos) && Objects.equals(level.getBlockState(pos), blockState) && Objects.equals(level.getBlockEntity(pos), entity)) {
                 return value;
             } else {
                 return null;
@@ -51,19 +55,14 @@ public class TreeCache {
             this.level = level;
             this.pos = pos;
             blockState = level.getBlockState(pos);
-        }
-
-        public void clear(BlockGetter level, BlockPos pos) {
-            if (level == this.level & pos.equals(this.pos)) {
-                value = null;
-                blockState = Blocks.AIR.defaultBlockState();
-            }
+            entity = level.getBlockEntity(pos);
         }
 
         public void invalidate() {
             level = null;
             value = null;
             blockState = Blocks.AIR.defaultBlockState();
+            entity = null;
         }
     }
 }
