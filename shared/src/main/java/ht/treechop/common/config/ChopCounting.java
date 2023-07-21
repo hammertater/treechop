06a7c6cc4f6103a5a443778b39042dba15cc4c6b
hammertater <74752031+hammertater@users.noreply.github.com>
2023-07-20 21:02:09 -0400

    Mishmash

--- shared/src/main/java/ht/treechop/common/config/ChopCounting.java
+++ shared/src/main/java/ht/treechop/common/config/ChopCounting.java
@@ -6,26 +6,26 @@ public class ChopCounting {
     private static final int[] cache = new int[CACHE_SIZE];
     private static int numCached = 1;
 
-    // Careful with early class loading
+    // Careful with early class loading; TODO: reset and clear cache on config reload
     private static final Rounder rounder = ConfigHandler.COMMON.chopCountRounding.get();
     private static final boolean canRequireMoreChopsThanBlocks = ConfigHandler.COMMON.canRequireMoreChopsThanBlocks.get();
     private static final ChopCountingAlgorithm algorithm = ConfigHandler.COMMON.chopCountingAlgorithm.get();
 
-    public static int calculate(int numBlocks) {
-        if (numBlocks < CACHE_SIZE) {
-            if (numCached < numBlocks) {
-                for (; numCached <= numBlocks; ++numCached) {
+    public static int calculate(int support) {
+        if (support < CACHE_SIZE) {
+            if (numCached <= support) {
+                for (; numCached <= support; ++numCached) {
                     cache[numCached] = recalculate(numCached);
                 }
             }
-            return cache[numBlocks];
+            return cache[support];
         } else {
-            return recalculate(numBlocks);
+            return recalculate(support);
         }
     }
 
-    private static int recalculate(int numBlocks) {
-        int count = Math.max(1, rounder.round(algorithm.calculate(numBlocks, rounder, canRequireMoreChopsThanBlocks)));
-        return canRequireMoreChopsThanBlocks ? count : Math.min(numBlocks, count);
+    private static int recalculate(int support) {
+        int count = Math.max(1, rounder.round(algorithm.calculate(support, rounder, canRequireMoreChopsThanBlocks)));
+        return canRequireMoreChopsThanBlocks ? count : Math.min(support, count);
     }
 }
