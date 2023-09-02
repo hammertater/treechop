36c6b6ccad76b584337c7e13219269612efb0028
hammertater <74752031+hammertater@users.noreply.github.com>
2023-09-01 23:39:37 -0400

    Fix Jade compat #221

--- fabric/src/main/java/ht/treechop/compat/Jade.java
+++ fabric/src/main/java/ht/treechop/compat/Jade.java
@@ -1,13 +1,10 @@
 package ht.treechop.compat;
 
 import ht.treechop.TreeChop;
-import ht.treechop.client.Client;
-import ht.treechop.client.settings.ClientChopSettings;
 import ht.treechop.common.block.ChoppedLogBlock;
 import ht.treechop.common.registry.FabricModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -15,7 +12,6 @@ import net.minecraft.world.phys.Vec2;
 import snownee.jade.api.*;
 import snownee.jade.api.config.IPluginConfig;
 import snownee.jade.api.ui.IElement;
-import snownee.jade.impl.ui.TextElement;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -63,23 +59,21 @@ public class Jade implements IWailaPlugin, IBlockComponentProvider {
 
     private static void changeBlockName(ITooltip tooltip, BlockAccessor accessor) {
         final ResourceLocation OBJECT_NAME_COMPONENT_KEY = new ResourceLocation("jade", "object_name");
-        try {
-            if (accessor.getBlockEntity() instanceof ChoppedLogBlock.MyEntity choppedEntity) {
-                Class.forName("snownee.jade.impl.ui.TextElement");
-                if (tooltip.get(OBJECT_NAME_COMPONENT_KEY).get(0) instanceof TextElement textElement) {
-                    if (textElement.component instanceof MutableComponent textComponent) {
-                        String choppedLogName = FabricModBlocks.CHOPPED_LOG.getName().getString();
-                        List<Component> siblings = textComponent.getSiblings();
-                        for (int i = 0, n = siblings.size(); i < n; ++i) {
-                            if (siblings.get(i).getString().matches(choppedLogName)) {
-                                siblings.set(i, WailaUtil.getPrefixedBlockName(choppedEntity));
-                                break;
-                            }
+        if (accessor.getBlockEntity() instanceof ChoppedLogBlock.MyEntity choppedEntity) {
+            IElement nameElement;
+            Component nameComponent;
+            if ((nameElement = tooltip.get(OBJECT_NAME_COMPONENT_KEY).get(0)) != null) {
+                if ((nameComponent = nameElement.getMessage()) != null) {
+                    String choppedLogName = FabricModBlocks.CHOPPED_LOG.getName().getString();
+                    List<Component> siblings = nameComponent.getSiblings();
+                    for (int i = 0, n = siblings.size(); i < n; ++i) {
+                        if (siblings.get(i).getString().matches(choppedLogName)) {
+                            siblings.set(i, WailaUtil.getPrefixedBlockName(choppedEntity));
+                            break;
                         }
                     }
                 }
             }
-        } catch (NullPointerException | ClassNotFoundException ignored) {
         }
     }
 
--- forge/src/main/java/ht/treechop/compat/Jade.java
+++ forge/src/main/java/ht/treechop/compat/Jade.java
@@ -1,13 +1,10 @@
 package ht.treechop.compat;
 
 import ht.treechop.TreeChop;
-import ht.treechop.client.Client;
-import ht.treechop.client.settings.ClientChopSettings;
 import ht.treechop.common.block.ChoppedLogBlock;
 import ht.treechop.common.registry.ForgeModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -15,7 +12,6 @@ import net.minecraft.world.phys.Vec2;
 import snownee.jade.api.*;
 import snownee.jade.api.config.IPluginConfig;
 import snownee.jade.api.ui.IElement;
-import snownee.jade.impl.ui.TextElement;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -42,7 +38,6 @@ public class Jade implements IWailaPlugin, IBlockComponentProvider {
 
         Level level = accessor.getLevel();
         BlockPos pos = accessor.getPosition();
-        ClientChopSettings chopSettings = Client.getChopSettings();
         boolean showNumBlocks = config.get(SHOW_TREE_BLOCKS);
         boolean showChopsRemaining = config.get(SHOW_NUM_CHOPS_REMAINING);
 
@@ -64,22 +59,21 @@ public class Jade implements IWailaPlugin, IBlockComponentProvider {
 
     private static void changeBlockName(ITooltip tooltip, BlockAccessor accessor) {
         final ResourceLocation OBJECT_NAME_COMPONENT_KEY = new ResourceLocation("jade", "object_name");
-        try {
-            if (accessor.getBlockEntity() instanceof ChoppedLogBlock.MyEntity choppedEntity) {
-                if (tooltip.get(OBJECT_NAME_COMPONENT_KEY).get(0) instanceof TextElement textElement) {
-                    if (textElement.component instanceof MutableComponent textComponent) {
-                        String choppedLogName = ForgeModBlocks.CHOPPED_LOG.get().getName().getString();
-                        List<Component> siblings = textComponent.getSiblings();
-                        for (int i = 0, n = siblings.size(); i < n; ++i) {
-                            if (siblings.get(i).getString().matches(choppedLogName)) {
-                                siblings.set(i, WailaUtil.getPrefixedBlockName(choppedEntity));
-                                break;
-                            }
+        if (accessor.getBlockEntity() instanceof ChoppedLogBlock.MyEntity choppedEntity) {
+            IElement nameElement;
+            Component nameComponent;
+            if ((nameElement = tooltip.get(OBJECT_NAME_COMPONENT_KEY).get(0)) != null) {
+                if ((nameComponent = nameElement.getMessage()) != null) {
+                    String choppedLogName = ForgeModBlocks.CHOPPED_LOG.get().getName().getString();
+                    List<Component> siblings = nameComponent.getSiblings();
+                    for (int i = 0, n = siblings.size(); i < n; ++i) {
+                        if (siblings.get(i).getString().matches(choppedLogName)) {
+                            siblings.set(i, WailaUtil.getPrefixedBlockName(choppedEntity));
+                            break;
                         }
                     }
                 }
             }
-        } catch (NullPointerException ignored) {
         }
     }
 
