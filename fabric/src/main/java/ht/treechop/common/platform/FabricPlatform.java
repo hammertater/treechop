822dbbf9fc6dd6e825a10c05818461a0bdd8e232
hammertater <74752031+hammertater@users.noreply.github.com>
2023-12-22 22:32:04 -0500

    Fabric work

--- fabric/src/main/java/ht/treechop/common/platform/FabricPlatform.java
+++ fabric/src/main/java/ht/treechop/common/platform/FabricPlatform.java
@@ -5,9 +5,12 @@ import ht.treechop.common.chop.ChopResult;
 import ht.treechop.common.chop.FellTreeResult;
 import ht.treechop.common.registry.FabricModBlocks;
 import net.fabricmc.api.EnvType;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -80,12 +83,12 @@ public class FabricPlatform implements Platform {
 
     @Override
     public ResourceLocation getResourceLocationForBlock(Block block) {
-        return Registry.BLOCK.getKey(block);
+        return BuiltInRegistries.BLOCK.getKey(block);
     }
 
     @Override
     public ResourceLocation getResourceLocationForItem(Item item) {
-        return Registry.ITEM.getKey(item);
+        return BuiltInRegistries.ITEM.getKey(item);
     }
 
     @Override
--- fabric/src/main/java/ht/treechop/common/registry/FabricModBlocks.java
+++ fabric/src/main/java/ht/treechop/common/registry/FabricModBlocks.java
@@ -4,25 +4,25 @@ import ht.treechop.TreeChop;
 import ht.treechop.common.block.FabricChoppedLogBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
 
 public class FabricModBlocks {
-    public static final TagKey<Block> CHOPPABLES = TagKey.create(Registry.BLOCK_REGISTRY, TreeChop.resource("choppables"));
+    public static final TagKey<Block> CHOPPABLES = TagKey.create(Registries.BLOCK, TreeChop.resource("choppables"));
 
     public static final Block CHOPPED_LOG = new FabricChoppedLogBlock(
-            FabricBlockSettings.of(
-                            Material.WOOD,
-                            MaterialColor.WOOD)
+            FabricBlockSettings.of()
+                    .mapColor(MapColor.WOOD)
+                    .instrument(NoteBlockInstrument.BASS)
                     .strength(2.0F)
                     .sound(SoundType.WOOD)
+                    .ignitedByLava()
     );
 
     public static final BlockEntityType<FabricChoppedLogBlock.MyEntity> CHOPPED_LOG_ENTITY = FabricBlockEntityTypeBuilder.create(FabricChoppedLogBlock.MyEntity::new, CHOPPED_LOG).build();
-
 }
