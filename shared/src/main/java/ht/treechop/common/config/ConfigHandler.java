fb709293b0a7c05a4c8ba46c39da0abe75c24e52
hammertater <74752031+hammertater@users.noreply.github.com>
2024-01-07 12:01:19 -0500

    Blacklist Twilight Forest's giant pickaxe #247

--- shared/src/main/java/ht/treechop/common/config/ConfigHandler.java
+++ shared/src/main/java/ht/treechop/common/config/ConfigHandler.java
@@ -458,12 +458,13 @@ public class ConfigHandler {
                     .defineList("items",
                             Arrays.asList(
                                     "botania:terra_axe",
-                                    "mekanism:atomic_disassembler",
                                     "@lumberjack",
-                                    "practicaltools:iron_greataxe",
-                                    "practicaltools:golden_greataxe",
+                                    "mekanism:atomic_disassembler",
                                     "practicaltools:diamond_greataxe",
-                                    "practicaltools:netherite_greataxe"),
+                                    "practicaltools:golden_greataxe",
+                                    "practicaltools:iron_greataxe",
+                                    "practicaltools:netherite_greataxe",
+                                    "twilightforest:giant_pickaxe"),
                             always -> true);
             builder.pop();
 
